5bc16bc5bc6ff0ca5617ba7168f3a5f7ffded537
inseong-at-347991859424 <mGJeesSgb5dxR4gbsge+wIC0JyPEVFXkl31XyAvTOIg=>
2021-03-30 11:01:57 +0900

    self-study Start!

---
+++ src/main/java/com/inseong/dispersion/DispersionApplication.java
@@ -0,0 +1,13 @@
+package com.inseong.dispersion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispersionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DispersionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inseong/dispersion/DispersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inseong.dispersion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispersionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
